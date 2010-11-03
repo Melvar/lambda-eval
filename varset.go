370450846a05d214d9a70d0b21366f58b7322d8d
Melvar Chen <Melvar.Chen@googlemail.com>
2010-11-03 22:17:02 +0100

    Changed and fixed VarSet, but still not yet used
    Turns out a Method reciever can’t be a pointer type.
    The methods should work now, needs testing though.

--- varset.go
+++ varset.go
@@ -1,41 +1,65 @@
 package λeval
 
 
-
 /* A VarSet represents a set of variables. */
-type VarSet *varlist
+type VarSet struct {
+	l *varlist
+}
+
+func Singleton(v Variable) VarSet {
+	return VarSet{cons(v, nil)}
+}
 
 /* Contains is the ∋ relation. It returns true if the Varset contains the
 Variable, and false if not. */
 func (s VarSet) Contains(v Variable) bool {
-	return (*varlist)(s).contains(v)
+	return s.l.contains(v)
 }
 
 /* Union is the ∪ operation. It returns a VarSet with all elements in either
 argument VarSet, or both. */
-func (s VarSet) Union(t Varset) Varset {
-	return (*varlist)(s).union((*varlist)(t))
+func (s VarSet) Union(t VarSet) VarSet {
+	return VarSet{s.l.union(t.l)}
+}
+
+/* Without is the ∖{·} operation. It returns the VarSet except for the
+specified element. */
+func (s VarSet) Without(v Variable) VarSet {
+	return VarSet{s.l.without(v)}
 }
 
 /* A varlist is the internal representation of a VarSet */
-type varlist struct{
+type varlist struct {
 	head Variable
 	tail *varlist
 }
 
 func cons(v Variable, t *varlist) *varlist {
-	return &varlist{ v, t }
+	return &varlist{v, t}
 }
 
-func (l varlist) contains(v Variable) bool {
-	return l.head == v || tail.contains(v)
+func (l *varlist) contains(v Variable) bool {
+	if l == nil {
+		return false
+	}
+	return l.head == v || l.tail.contains(v)
 }
 
-func (l varlist) union(m varlist) *varlist {
+func (l *varlist) union(m *varlist) *varlist {
 	if m == nil {
 		return l
+	} else if l == nil {
+		return m
 	}
-	if l.contains(m.head) {return l.union(m.tail)}
+	if l.contains(m.head) {
+		return l.union(m.tail)
+	}
 	return cons(m.head, l).union(m.tail)
 }
 
+func (l *varlist) without(v Variable) *varlist {
+	if l.head == v {
+		return l.tail
+	}
+	return cons(l.head, l.tail.without(v))
+}
