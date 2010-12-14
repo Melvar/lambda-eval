1e7d93ee2c7024cec2453c3e1ff722a17d5cf196
Melvar Chen <Melvar.Chen@googlemail.com>
2010-12-14 11:35:17 +0100

    corrected isSpace case in Lexer.Next()

--- parser.go
+++ parser.go
@@ -90,37 +90,37 @@ func (l *Lexer) Next() os.Error {
 		return l.current
 	}
 	switch nextrune {
-		case Lambda, LPar, RPar, Dot:
-			l.pos += nextwidth
-			l.current = Token{nextrune, nil}
+	case Lambda, LPar, RPar, Dot:
+		l.pos += nextwidth
+		l.current = Token{nextrune, nil}
+		return nil
+	default:
+		switch {
+		case unicode.IsSpace(nextrune):
+		case unicode.IsLetter(nextrune):
+			var endVar = bytes.IndexFunc(l.buf[l.pos:], nonVarRune)
+			for endVar < 0 {
+				var newbuf = make([]byte, 2*len(l.buf))
+				copy(newbuf, l.buf[l.pos:])
+				if n, err := io.ReadFull(l.rd, l.buf[len(l.buf)-l.pos:]); n < l.pos {
+					if !(err == io.ErrUnexpectedEOF || err == os.EOF) {
+						l.current = Token{Error, &err.String()}
+					}
+					l.rdEmpty = true
+					l.buf = l.buf[:len(l.buf)-l.pos+n]
+					return err
+				}
+				l.pos = 0
+				l.buf = newbuf
+				endVar = bytes.IndexFunc(l.buf[l.pos:], nonVarRune)
+			}
+			l.pos = endVar
+			l.current = Token{Var, &string(l.buf[l.pos:endVar])}
 			return nil
 		default:
-			switch {
-				case unicode.IsSpace:
-				case unicode.IsLetter(nextrune):
-					var endVar = bytes.IndexFunc(l.buf[l.pos:], nonVarRune)
-					for (endVar < 0) {
-						var newbuf = make([]byte, 2*len(l.buf))
-						copy(newbuf, l.buf[l.pos:]
-						if n, err := io.ReadFull(l.rd, l.buf[len(l.buf)-l.pos:]); n < l.pos {
-							if !(err == io.ErrUnexpectedEOF || err == os.EOF) {
-								l.current = Token{Error, &err.String()}
-							}
-							l.rdEmpty = true
-							l.buf = l.buf[:len(l.buf)-l.pos+n]
-							return err
-						}
-						l.pos = 0
-						l.buf = newbuf
-						endVar = bytes.IndexFunc(l.buf[l.pos:], nonVarRune)
-					}
-					l.pos = endVar
-					l.current = Token{Var, &string(l.buf[l.pos:endVar])}
-					return nil
-				default:
-					l.pos += nextwidth
-					l.current = Token{Error, &string(nextrune)}
-					return l.current
-			}
+			l.pos += nextwidth
+			l.current = Token{Error, &string(nextrune)}
+			return l.current
+		}
 	}
 }
